f43aecdfc9a49eb6e9ecb7c7fd4d3f47295ab6f6
ngochung1809 <ngochung1809@kali-linux.ngochung1809>
2023-10-27 09:17:53 +0700

    update cka

---
+++ container-docker-onprem/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get install ca-certificates curl gnupg -y
+curl -Ls "https://sbom.k8s.io/$(curl -Ls https://dl.k8s.io/release/latest.txt)/release"  | awk '/Package: registry.k8s.io\// {print $3}'
+
+curl -LO https://dl.k8s.io/release/v1.26.7/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client --output=yaml
+kubectl cluster-info
+
+# Install container runtime
+
+sudo apt-get update
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo chmod 666 /var/run/docker.sock
+# Install kubeadm
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.26/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.26/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+# Setup Calico Network
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/tigera-operator.yaml
--- on-prem/configs/join.sh
+++ on-prem/configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.0.10:6443 --token ffsc2d.4myzv8z1t1zfr618 --discovery-token-ca-cert-hash sha256:d02b0c2a5143c1cbf27ef86df5ee8752ecf0d5f2fbd5435990a5f74765413de2 
+kubeadm join 10.0.0.10:6443 --token j5yqoq.ms4pimwrvxckn0x3 --discovery-token-ca-cert-hash sha256:b8f523ba91e6b7e2c633b6dadceb888c25dd3733eea530b341a09962d868b7be 
